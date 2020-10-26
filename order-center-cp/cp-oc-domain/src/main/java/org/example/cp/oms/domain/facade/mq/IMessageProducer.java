0d178dfbbcc60d8c1489212b185e35599f4333cc
Funky Gao <funky.gao@gmail.com>
2020-10-26 22:57:05 +0800

    how domain layer interact with infrastructure layer

--- order-center-cp/cp-oc-domain/src/main/java/org/example/cp/oms/domain/facade/mq/IMessageProducer.java
+++ order-center-cp/cp-oc-domain/src/main/java/org/example/cp/oms/domain/facade/mq/IMessageProducer.java
@@ -4,6 +4,13 @@ import org.example.cp.oms.domain.model.OrderMain;
 
 import javax.validation.constraints.NotNull;
 
+/**
+ * MQ的使用方式通过接口形式定义在domain层，Infrastructure来实现.
+ *
+ * <p>一种依赖倒置模式</p>
+ * <p>这样，domain层才能主宰世界，成为系统的中心：它要求Infrastructure做到什么</p>
+ * <p>至于如何做到，domain层不关心：那是技术的事情，domain只负责业务逻辑</p>
+ */
 public interface IMessageProducer {
 
     void produce(@NotNull OrderMain orderModel);
