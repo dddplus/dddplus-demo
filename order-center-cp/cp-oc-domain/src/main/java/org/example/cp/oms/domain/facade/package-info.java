5caa97f322f3213ab981c7ef3fa0455b845b0027
Funky Gao <funky.gao@gmail.com>
2020-10-26 23:06:24 +0800

    fix typo

--- order-center-cp/cp-oc-domain/src/main/java/org/example/cp/oms/domain/facade/package-info.java
+++ order-center-cp/cp-oc-domain/src/main/java/org/example/cp/oms/domain/facade/package-info.java
@@ -1,5 +1,5 @@
 /**
- * DDD Domain层和Infrastructure的粘合剂：通过倒置依赖.
+ * DDD Domain层和Infrastructure的粘合剂：通过依赖倒置.
  *
  * <p>domain层声明需要基础设施层实现的接口：RPC, DB, Cache, MQ等.</p>
  * <p>为了方便产品人员查看领域层代码，梳理业务，统一放在facade package，减少对产品同学的干扰.</p>
