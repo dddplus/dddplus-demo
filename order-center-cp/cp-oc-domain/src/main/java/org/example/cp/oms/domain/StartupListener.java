e488a260c14395d50f85a4c09d0fcc71628c2a23
Funky Gao <funky.gao@gmail.com>
2020-11-05 14:11:28 +0800

    demonstrate how to use DomainArtifacts

--- order-center-cp/cp-oc-domain/src/main/java/org/example/cp/oms/domain/StartupListener.java
+++ order-center-cp/cp-oc-domain/src/main/java/org/example/cp/oms/domain/StartupListener.java
@@ -1,8 +1,12 @@
 package org.example.cp.oms.domain;
 
+import io.github.dddplus.runtime.IStartupListener;
+import io.github.dddplus.runtime.registry.DomainArtifacts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import io.github.dddplus.runtime.IStartupListener;
+
+import java.util.List;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -11,5 +15,30 @@ public class StartupListener implements IStartupListener {
     @Override
     public void onStartComplete() {
         log.info("DDD framework booted!");
+
+        DomainArtifacts artifacts = DomainArtifacts.getInstance();
+        log.info("Domains:");
+        for (DomainArtifacts.Domain domain : artifacts.getDomains()) {
+            log.info("    {}", domain.getName());
+        }
+
+        log.info("Specifications:");
+        for (DomainArtifacts.Specification specification : artifacts.getSpecifications()) {
+            log.info("    {}", specification.getName());
+        }
+
+        log.info("Steps:");
+        for (Map.Entry<String, List<DomainArtifacts.Step>> entry : artifacts.getSteps().entrySet()) {
+            log.info("    activity: {}", entry.getKey());
+            for (DomainArtifacts.Step step : entry.getValue()) {
+                log.info("        step: {}, tags:{}", step.getCode(), step.getTags());
+            }
+        }
+
+        log.info("Extensions:");
+        for (DomainArtifacts.Extension extension : artifacts.getExtensions()) {
+            log.info("    {}: partners:{}, patterns:{}", extension.getExt().getCanonicalName(), extension.getPartners(), extension.getPatterns());
+        }
+
     }
 }
