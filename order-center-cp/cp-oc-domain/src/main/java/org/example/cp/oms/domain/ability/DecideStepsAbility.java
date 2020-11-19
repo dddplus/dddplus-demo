eb64847f33ea9c66c82cda990b51e37d08342fbd
Funky Gao <funky.gao@gmail.com>
2020-11-19 14:51:19 +0800

    use BaseDecideStepsAbility

--- order-center-cp/cp-oc-domain/src/main/java/org/example/cp/oms/domain/ability/DecideStepsAbility.java
+++ order-center-cp/cp-oc-domain/src/main/java/org/example/cp/oms/domain/ability/DecideStepsAbility.java
@@ -1,25 +1,11 @@
 package org.example.cp.oms.domain.ability;
 
+import io.github.dddplus.annotation.DomainAbility;
+import io.github.dddplus.runtime.BaseDecideStepsAbility;
 import org.example.cp.oms.domain.CoreDomain;
 import org.example.cp.oms.spec.DomainAbilities;
 import org.example.cp.oms.spec.model.IOrderMain;
-import io.github.dddplus.annotation.DomainAbility;
-import io.github.dddplus.ext.IDecideStepsExt;
-import io.github.dddplus.runtime.BaseDomainAbility;
-
-import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @DomainAbility(domain = CoreDomain.CODE, name = "动态决定领域步骤的能力", tags = DomainAbilities.decideSteps)
-public class DecideStepsAbility extends BaseDomainAbility<IOrderMain, IDecideStepsExt> {
-
-    public List<String> decideSteps(@NotNull IOrderMain model, String activityCode) {
-        return firstExtension(model).decideSteps(model, activityCode);
-    }
-
-    @Override
-    public IDecideStepsExt defaultExtension(@NotNull IOrderMain model) {
-        // 没有默认的扩展点实现
-        return null;
-    }
+public class DecideStepsAbility extends BaseDecideStepsAbility<IOrderMain> {
 }
